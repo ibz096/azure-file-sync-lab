80069285df0d9571b5213f87038b6bf111b2dcf8
Ibrahim Ahmed <13389823+ibz096@users.noreply.github.com>
2025-06-11 23:45:27 -0400

    Changed Modules

--- registerToStorageSyncService.ps1
+++ registerToStorageSyncService.ps1
@@ -2,7 +2,8 @@ param (
     [string]$ResourceGroupName,
     [string]$SyncServiceName
 )
-Install-Module -Name Az -Force
+Install-Module -Name Az.Accounts -Force -Scope AllUsers
+Install-Module -Name Az.StorageSync -Force -Scope AllUsers
 
 Connect-AzAccount -identity
 
