6e4d99a9f2b1c48d6eae9276c1a325fc60227328
Ibrahim Ahmed <13389823+ibz096@users.noreply.github.com>
2025-06-14 16:11:35 -0400

    Added test logging

--- registerToStorageSyncService.ps1
+++ registerToStorageSyncService.ps1
@@ -2,6 +2,10 @@ param (
     [string]$ResourceGroupName,
     [string]$StorageSyncServiceName
 )
+
+write-host $ResourceGroupName
+write-host $StorageSyncServiceName
+
 Connect-AzAccount -identity
 
 Register-AzStorageSyncServer -ResourceGroupName $ResourceGroupName -StorageSyncServiceName $StorageSyncServiceName
