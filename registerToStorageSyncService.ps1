bc77a204a330aa885570244e5c6ca86076445d95
Ibrahim Ahmed <13389823+ibz096@users.noreply.github.com>
2025-06-13 22:23:59 -0400

    Modified to use PSResource Module

--- registerToStorageSyncService.ps1
+++ registerToStorageSyncService.ps1
@@ -2,8 +2,9 @@ param (
     [string]$ResourceGroupName,
     [string]$SyncServiceName
 )
-Install-Module -Name Az.Accounts -Force -Scope AllUsers
-Install-Module -Name Az.StorageSync -Force -Scope AllUsers
+
+Install-Module -Name Microsoft.PowerShell.PSResourceGet -Repository PSGallery
+Install-PSResource -Name Az.Accounts, Az.StorageSync -Force -Scope AllUsers
 
 Connect-AzAccount -identity
 
