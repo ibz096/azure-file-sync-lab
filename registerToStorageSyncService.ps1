72ef101c0eecb9f36425ef4e0de5f618cd8e6ad7
Ibrahim Ahmed <13389823+ibz096@users.noreply.github.com>
2025-06-14 16:50:40 -0400

    Updated Logging

--- registerToStorageSyncService.ps1
+++ registerToStorageSyncService.ps1
@@ -3,8 +3,11 @@ param (
     [string]$StorageSyncServiceName
 )
 
-write-host $ResourceGroupName
-write-host $StorageSyncServiceName
+write-host "Registering to Storage Sync Service..." | Out-file -FilePath "C:\temp\registerToStorageSyncService.log" -Append
+write-host "Resource Group Name: " | Out-file -FilePath "C:\temp\registerToStorageSyncService.log" -Append
+write-host $ResourceGroupName | Out-file -FilePath "C:\temp\registerToStorageSyncService.log" -Append
+write-host "Storage Sync Service Name: " | Out-file -FilePath "C:\temp\registerToStorageSyncService.log" -Append
+write-host $StorageSyncServiceName | Out-file -FilePath "C:\temp\registerToStorageSyncService.log" -Append
 
 Connect-AzAccount -identity
 
