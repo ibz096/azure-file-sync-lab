231765390cc2310a8dccb599fa0eb572985a95a5
Ibrahim Ahmed <13389823+ibz096@users.noreply.github.com>
2025-07-09 21:57:46 -0400

    Clean up

--- scripts/registerToStorageSyncService.ps1
+++ scripts/registerToStorageSyncService.ps1
@@ -3,11 +3,6 @@ param (
     [string]$StorageSyncServiceName
 )
 "[$(Get-Date)] Args: ResourceGroup=$ResourceGroupName, SyncService=$StorageSyncServiceName" | Out-File -Append C:\Temp\setup.log
-write-host "Registering to Storage Sync Service..." | Out-file -FilePath "C:\temp\registerToStorageSyncService.log" -Append
-write-host "Resource Group Name: " | Out-file -FilePath "C:\temp\registerToStorageSyncService.log" -Append
-write-host $ResourceGroupName | Out-file -FilePath "C:\temp\registerToStorageSyncService.log" -Append
-write-host "Storage Sync Service Name: " | Out-file -FilePath "C:\temp\registerToStorageSyncService.log" -Append
-write-host $StorageSyncServiceName | Out-file -FilePath "C:\temp\registerToStorageSyncService.log" -Append
 
 Connect-AzAccount -identity
 
